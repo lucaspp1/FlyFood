c7e5955dee42edb8080e92232aca58ce3818d1f5
lucas coutinho <lucaspp1@ucl.br>
2019-10-12 14:44:46 -0300

    Commit inicial

---
+++ FlyFoodAplication/FlyFood/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlyFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
