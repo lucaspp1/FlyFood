dfa53600a3f378d3e22135c3ef6fdf60c57c8954
lucas coutinho <lucaspp1@ucl.br>
2019-10-12 16:09:48 -0300

    Classes iniciais adicionadas

---
+++ FlyFoodAplication/FlyFood/Model/Voo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyFood.Model
+{
+    public class Voo
+    {
+        public DateTime Decolagem { get; set; }
+        public float TempoVoo { get; set; }
+        public string Aviao{ get; set; }
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+        public string NomePiloto { get; set; }
+        public int Id { get; set; }
+
+    }
+}
