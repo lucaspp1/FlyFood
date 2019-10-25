c214696965d3c9b716a731489d0d03bd3e58b405
thamyris <thamyrisoliveira@ucl.br>
2019-10-24 21:31:57 -0300

    teste

--- FlyFoodAplication/FlyFood/TelaCliente.cs
+++ FlyFoodAplication/FlyFood/TelaCliente.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlyFood.DAO.Service;
+using FlyFood.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,24 +8,197 @@ namespace FlyFood
 {
     class TelaCliente
     {
-        public void MostrarTelaCliente()
+        /* MÉTODOS DE AMD */
+        private void cadastrarVoo()
         {
-            string menu = @"
-seleciona uma das opções:
-    1) Escolher um voo
-    2) Entrar um Voo
-    3) Sair
-opção: ";
-
+            Voo voo = new Voo();
+            try
+            {
+                Console.WriteLine("Insira a data de decolagem");
+                voo.Decolagem = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Insira a duração em hroas: ");
+                voo.TempoVoo = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o ");
+                voo.Aviao = Console.ReadLine();
+                Console.WriteLine("Insira o ");
+                voo.Origem = Console.ReadLine();
+                Console.WriteLine("Insira o ");
+                voo.Destino = Console.ReadLine();
+                Console.WriteLine("Insira o ");
+                voo.NomePiloto = Console.ReadLine();
+                new FileHelper<Voo>().Insert(voo, out string _ );
+                Console.WriteLine("voo Inserido co sucesso");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao cadastrar voo: " + ex.Message);
+            }
         }
+
+        private void inserirLancheVoo()
+        {
+            List<Voo> listaVoo = new FileHelper<Voo>().select();
+            List<Lanche> listaLanche = new FileHelper<Lanche>().select();
+            Lanche_Voo lancheVoo = new Lanche_Voo();
+            try
+            {
+                Console.WriteLine("  Selecione um voo ");
+                foreach (Voo voo in listaVoo)
+                {
+                    Console.WriteLine( $" {voo.Id} -`{voo.Decolagem} até ${voo.Destino} no dia ${voo.Decolagem.ToString("dd/mm/yyyy")} " );
+                }
+                int idVoo = int.Parse(Console.ReadLine());
+                while ( listaVoo.FindAll( o => o.Id == idVoo).Count <= 0 )
+                {
+                    Console.WriteLine("  Selecione um voo Válido ");
+                    idVoo = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("  Selecione um lanche ");
+                foreach (Lanche lanche in listaLanche)
+                {
+                    Console.WriteLine($" {lanche.Id} - {lanche.Nome} ");
+                }
+                int idLanche = int.Parse(Console.ReadLine());
+                while (listaLanche.FindAll(o => o.Id == idLanche).Count <= 0)
+                {
+                    Console.WriteLine("  Selecione um lanche Válido ");
+                    idVoo = int.Parse(Console.ReadLine());
+                }
+                lancheVoo.Lanche = idLanche;
+                lancheVoo.Voo = idLanche;
+
+                if (new FileHelper<Lanche_Voo>().select().FindAll( o => o.Voo == idVoo && o.Lanche == idLanche ).Count > 0)
+                {
+                    Console.WriteLine("  Lanche já inserido no voo ");
+                }
+                else
+                {
+                    Console.WriteLine("  Insira a quantidade de lanche ");
+                    int quantidade = int.Parse(Console.ReadLine());
+                    lancheVoo.Quantidade = quantidade;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao cadastrar lanche no Voo: " + ex.Message);
+            }
+        }
+
+        private void cadastrarLanche()
+        {
+            Lanche lanche = new Lanche();
+            try
+            {
+                Console.WriteLine("Insira o nome do produto: ");
+                lanche.Nome = Console.ReadLine();
+                Console.WriteLine("Insira o nome do produto: ");
+                lanche.Preco = float.Parse(Console.ReadLine());
+                new FileHelper<Lanche>().Insert(lanche, out string _);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao cadastrar lanche: " + ex.Message);
+            }
+        }
+
         public void MostrarTelaAdm()
         {
             string menu = @"
 seleciona uma das opções:
     1) Cadastrar Um voo
     2) Cadastrar Lanche
+    3) Adicionar Lanche para Voo
     3) Sair
 opção: ";
+            string opcao = "";
+            while (opcao != "3")
+            {
+                Console.WriteLine(menu);
+                opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1":
+                        cadastrarVoo();
+                        break;
+                    case "2":
+                        cadastrarLanche();
+                        break;
+                    case "3":
+                        inserirLancheVoo();
+                        break;
+                    case "4":
+                        Console.WriteLine("Logou :)");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inváliad");
+                        break;
+                }
+
+            }
         }
+        
+        public void ComprarPassagem()
+        {
+            List<Voo> listaVoo = new FileHelper<Voo>().select();
+            List<Passagem> passagensUsuario = new FileHelper<Passagem>().select().FindAll(o => o.Cliente == Program.clienteLogado.Id);
+            List<Voo> listaVooComprados = listaVoo.FindAll( voo => passagensUsuario.Exists( passagem => passagem.Voo == voo.Id) );
+            listaVoo.RemoveAll(voo => listaVooComprados.Exists( vooComprado => vooComprado.Id == voo.Id));
+            if (listaVoo.Count > 0)
+            {
+                Console.WriteLine("Selecione um Voo");
+                foreach (Voo voo in listaVoo)
+                {
+                    Console.WriteLine($" {voo.Id} - {voo.Origem} até {voo.Destino} ");
+                }
+                int idVoo = int.Parse(Console.ReadLine());
+                while ( !listaVoo.Exists( o => o.Id == idVoo) )
+                {
+                    Console.WriteLine("Digite um valor válido");
+                }
+                idVoo = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Nenhum Voo disponivel");
+            }
+        }
+        public void entrarVoo()
+        {
+
+        }
+
+        /* MÉTODOS DO CLIENTE */
+        public void MostrarTelaCliente()
+        {
+            string menu = @"
+seleciona uma das opções:
+    1) comprar Passagem um voo
+    2) Entrar um Voo
+    3) Sair
+opção: ";
+            string opcao = "";
+            while (opcao != "3")
+            {
+                Console.WriteLine(menu);
+                opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1":
+                        ComprarPassagem();
+                        break;
+                    case "2":
+                        entrarVoo();
+                        break;
+                    case "3":
+                        Console.WriteLine("Logou :)");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+            }
+        }
+
     }
 }
